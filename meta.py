c4f7b6f6e96e387355a3f79b7ae66f3140aa0c85
ymcidence <ym_zmxncbv@hotmail.com>
2021-03-12 23:52:39 +0800

    initial

---
+++ meta.py
@@ -0,0 +1,3 @@
+import os
+
+ROOT_PATH = os.path.abspath(__file__)[:os.path.abspath(__file__).rfind(os.path.sep)]
--- util/data/loader.py
+++ util/data/loader.py
@@ -2,7 +2,40 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import os
+from meta import ROOT_PATH
 
-cifar = tfds.load('cifar100')
 
-print(1)
+def load_cifar100():
+    data_dir = os.path.join(ROOT_PATH, 'data', 'cifar100')
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    data = tfds.load('cifar100', data_dir=data_dir)
+    return data
+
+
+def load_cifar_feature():
+    def data_parser(tf_example: tf.train.Example):
+        feat_dict = {'id': tf.io.FixedLenFeature([], tf.int64),
+                     'feat': tf.io.FixedLenFeature([4096], tf.float32),
+                     'label': tf.io.FixedLenFeature([10], tf.float32)}
+        features = tf.io.parse_single_example(tf_example, features=feat_dict)
+
+        _id = tf.cast(features['id'], tf.int32)
+        _feat = tf.cast(features['feat'], tf.float32)
+        _label = tf.cast(tf.argmax(features['label']), tf.int32)
+        return {'id': _id,
+                'image': _feat,
+                'label': _label
+                }
+
+    train_name = os.path.join(ROOT_PATH, 'data', 'cifar_feat', 'train.tfrecords')
+    test_name = train_name.replace('train.tfrecords', 'test.tfrecords')
+    train_data = tf.data.TFRecordDataset(train_name).map(data_parser, num_parallel_calls=50)
+    test_data = tf.data.TFRecordDataset(test_name).map(data_parser, num_parallel_calls=50)
+    return {'train': train_data,
+            'test': test_data}
+
+
+if __name__ == '__main__':
+    load_cifar100()
