bc0c08f814b0c443f5545ec3256f39e86e6dd6fd
yumishen <ymcidence@gmail.com>
2021-03-16 15:59:02 +0800

    12345678

--- util/config.py
+++ util/config.py
@@ -9,7 +9,7 @@ parser.add_argument('-k', '--k', default=10)
 parser.add_argument('-t', '--temp', default=0.9)
 parser.add_argument('-e', '--encoder', default='linear')
 parser.add_argument('-a', '--assigner', default='soft')
-parser.add_argument('-g', '--gumbel_temp', default=0.5,
+parser.add_argument('-g', '--gumbel_temp', default=0.3,
                     help='temp of the gumbel trick. Set it <0 to disable stochasticity')
 parser.add_argument('-n', '--set_name', default='cifar_feat', help='dataset')
 parser.add_argument('-tn', '--task_name', default='default', help='task name')
