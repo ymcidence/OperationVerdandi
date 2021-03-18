6a05829fb809196256541d662a10b0ed8c46e30e
yumishen <ymcidence@gmail.com>
2021-03-18 11:24:20 +0800

    tbh-like baseline

--- util/config.py
+++ util/config.py
@@ -6,7 +6,7 @@ parser.add_argument('-b', '--batch_size', default=512, type=int, help='batch_siz
 parser.add_argument('-i', '--max_iter', default=200000, help='iter')
 parser.add_argument('-d', '--d_model', default=512, help='emb size')
 parser.add_argument('-k', '--k', default=10)
-parser.add_argument('-t', '--temp', default=1)
+parser.add_argument('-t', '--temp', default=1, type=float)
 parser.add_argument('-e', '--encoder', default='linear')
 parser.add_argument('-a', '--assigner', default='soft')
 parser.add_argument('-g', '--gumbel_temp', default=0.3, type=float,
