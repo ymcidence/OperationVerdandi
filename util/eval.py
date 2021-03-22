7bca50710d3adad0ec0648e3875671588226af99
yumishen <ymcidence@gmail.com>
2021-03-22 15:07:31 +0800

    12345678

--- util/eval.py
+++ util/eval.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 import numpy as np
 from scipy.optimize import linear_sum_assignment as linear_assignment
 from sklearn.metrics.cluster import adjusted_rand_score, normalized_mutual_info_score, silhouette_score
+
+
 # import tensorflow as tf
 
 
@@ -18,6 +20,17 @@ def purity(feat, semantic, pred):
     return 0
 
 
+def exception_handler(f):
+    def rslt(*args):
+        try:
+            return f(*args)
+        except ValueError:
+            return 0
+
+    return rslt
+
+
+@exception_handler
 def acc(semantic, pred):
     """
     unsupervised clustering accuracy
@@ -26,17 +39,16 @@ def acc(semantic, pred):
     :return:
     """
     assert pred.size == semantic.size
-    try:
-        d = max(pred.max(), semantic.max()) + 1
-        w = np.zeros((d, d), dtype=np.int64)
-        for i in range(pred.size):
-            w[pred[i], semantic[i]] += 1
-        ind = linear_assignment(np.max(w) - w)
-        return sum([w[i, j] for i, j in zip(ind[0], ind[1])]) * 1.0 / pred.size, w
-    except ValueError:
-        return 0
+
+    d = max(pred.max(), semantic.max()) + 1
+    w = np.zeros((d, d), dtype=np.int64)
+    for i in range(pred.size):
+        w[pred[i], semantic[i]] += 1
+    ind = linear_assignment(np.max(w) - w)
+    return sum([w[i, j] for i, j in zip(ind[0], ind[1])]) * 1.0 / pred.size, w
 
 
+@exception_handler
 def nmi(semantic, pred):
     """
     NMI
@@ -47,6 +59,7 @@ def nmi(semantic, pred):
     return normalized_mutual_info_score(semantic, pred)
 
 
+@exception_handler
 def ari(semantic, pred):
     """
     ARI
@@ -57,6 +70,7 @@ def ari(semantic, pred):
     return adjusted_rand_score(semantic, pred)
 
 
+@exception_handler
 def silhouette_coefficient(feat, pred, metric='euclidean'):
     return silhouette_score(feat, pred, metric=metric)
 
