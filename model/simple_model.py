c1d45392e93227e2cc90ddc8aa067aa758d8ec53
yumishen <ymcidence@gmail.com>
2021-03-29 17:40:11 +0800

    12345678

--- model/simple_model.py
+++ model/simple_model.py
@@ -16,10 +16,11 @@ class SimpleModel(tf.keras.Model):
         self.q = conf.k * conf.q
         self.k = conf.k
 
-        self.context = tf.Variable(tf.initializers.GlorotNormal()([conf.k, conf.d_model]), trainable=True,
-                                   dtype=tf.float32, name='ContextEmb')
+        _context = tf.Variable(tf.initializers.GlorotNormal()([conf.k, conf.d_model]), trainable=True,
+                               dtype=tf.float32, name='ContextEmb')
+        self.context = tf.nn.l2_normalize(_context)
         self.encoder = get_encoder(conf)
-        self.ln_n = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        # self.ln_n = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         # self.ln_k = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.decoder = tf.keras.Sequential([
             tf.keras.layers.Dense(2048, activation=tf.nn.relu),
@@ -32,23 +33,24 @@ class SimpleModel(tf.keras.Model):
 
     def call(self, inputs, training=True, mask=None, step=-1):
         feat = self.encoder(inputs, training=training)
+        feat = tf.nn.l2_normalize(feat)
 
         # [N K] agg
         _kn = tf.matmul(self.context, feat, transpose_b=True)  # [K N]
         assign_n = gumbel_softmax(tf.transpose(_kn), self.gumbel_temp, hard=False)  # [N K]
         _assign_n = gumbel_softmax(tf.transpose(_kn), self.gumbel_temp, hard=True)
         agg_n = assign_n @ self.context  # [N D]
-        agg_n = self.ln_n(agg_n, training=training)
+        # agg_n = self.ln_n(agg_n, training=training)
         pred = self.decoder(agg_n, training=training)
 
         # [K N] agg
         heat_map = _kn
-        eps = tf.random.uniform(tf.shape(heat_map), minval=0, maxval=.5)  # [K N]
+        eps = tf.random.uniform(tf.shape(heat_map), minval=.3, maxval=.7)  # [K N]
         assign_k, _ = binary_activation(heat_map, eps)  # [K N]
 
         def split_agg(_assign, _feat):
             agg_k = _assign @ _feat  # [K D] note that this aggregation is still unnormalized
-            normalizer = tf.reduce_mean(_assign, axis=1, keepdims=True) + 1e-8  # [K 1]
+            normalizer = tf.reduce_sum(_assign, axis=1, keepdims=True) + 1e-8  # [K 1]
             agg_k = agg_k / normalizer
             agg_k = tf.nn.l2_normalize(agg_k)  # self.ln_k(agg_k, training=training)
             return agg_k
