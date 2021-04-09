8c9d47a9750386334ecb091a8517f2d9c5e648d5
yumishen <ymcidence@gmail.com>
2021-04-09 14:02:04 +0800

    12345678

--- model/moco.py
+++ model/moco.py
@@ -31,7 +31,7 @@ class MoCo(tf.keras.Model):
         self.gumbel_temp = conf.gumbel_temp
         self.base_1 = BaseModel(conf)
         self.base_2 = BaseModel(conf)
-        self.fc_1 = tf.keras.layers.Dense(conf.d_model)
+        # self.fc_1 = tf.keras.layers.Dense(conf.d_model)
         _queue_n = tf.Variable(tf.initializers.GlorotUniform()([self.l, conf.d_model]), trainable=False,
                                dtype=tf.float32, name='QueueN')
 
@@ -78,16 +78,16 @@ class MoCo(tf.keras.Model):
 
     @property
     def trainable_scope(self):
-        return self.base_1.trainable_variables + self.fc_1.trainable_variables
+        return self.base_1.trainable_variables #+ self.fc_1.trainable_variables
 
     def cross_rep(self, feat, context, stochastic=1, step=-1):
         # [N K] agg
         _kn = tf.matmul(context, feat, transpose_b=True)  # [K N]
         assign_n = gumbel_softmax(tf.transpose(_kn), self.gumbel_temp, hard=False)  # [N K]
         _assign_n = gumbel_softmax(tf.transpose(_kn), self.gumbel_temp, hard=True)
-        agg_n = assign_n @ context  # [N D]
-        agg_n = tf.nn.l2_normalize(agg_n, axis=1) + tf.nn.l2_normalize(self.fc_1(feat), axis=1) * .1
-        agg_n = tf.nn.l2_normalize(agg_n, axis=1)
+        # agg_n = assign_n @ context  # [N D]
+        # agg_n = tf.nn.l2_normalize(agg_n, axis=1)  # + tf.nn.l2_normalize(self.fc_1(feat), axis=1) * .1
+        agg_n = tf.nn.l2_normalize(feat, axis=1)
 
         # [K N] agg
 
